095f28c9016d9d1c982e0ba8f81b83c37fe39ab1
Tori Meyers <159385699+hecksgirl@users.noreply.github.com>
2024-09-24 20:00:37 -0500

    Collectible
    random weight & size, adds weight to player and base

--- Squorror/Assets/Scripts/Collectible.cs
+++ Squorror/Assets/Scripts/Collectible.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public float collectibleWeight;
+    public float minCollectibleSize;
+    public float maxCollectibleSize;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float randSize = Random.Range(minCollectibleSize, maxCollectibleSize);
+        Vector3 newSize = new Vector3(randSize, randSize, randSize);
+        transform.localScale = newSize;
+        collectibleWeight = randSize * randSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            GetCollected();
+        }
+    }
+
+   // private void OnCollisionEnter(Collision collision)
+   // {
+   //   GetCollected(); //Make it so player collision only???
+   // }
+ 
+    public void GetCollected()
+    {
+        GameManager.Instance.AddWeightToPlayer(collectibleWeight);
+        Destroy(gameObject);
+    }
+
+    public void SlowDownPlayer()
+    {
+
     }
 }
