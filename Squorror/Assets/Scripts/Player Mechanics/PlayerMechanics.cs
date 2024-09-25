5299500fabd82ae8374550f1314e7a3f7c912d97
WormAround <96322100+WormAround@users.noreply.github.com>
2024-09-24 19:12:11 -0500

    handle movement

--- Squorror/Assets/Scripts/Player Mechanics/PlayerMechanics.cs
+++ Squorror/Assets/Scripts/Player Mechanics/PlayerMechanics.cs
@@ -21,7 +21,7 @@ public class PlayerMechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovementInput();
     }
 
     void HandleMovementInput()
