e6d6c5d03b4134fa7685261671c61a28442519ab
WormAround <96322100+WormAround@users.noreply.github.com>
2024-09-24 19:01:16 -0500

    Movement Code part 1

--- Squorror/Assets/Scripts/Player Mechanics/PlayerMechanics.cs
+++ Squorror/Assets/Scripts/Player Mechanics/PlayerMechanics.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerMechanics : MonoBehaviour
 {
+    public CharacterController characterController;
+    public float speed;
+    public int itemsCollected;
+    bool isDead;
+    public float maxHealth;
+    public float currentHealth;
+    public float jumpHeight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +23,17 @@ public class PlayerMechanics : MonoBehaviour
     {
         
     }
+
+    void HandleMovementInput()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 moveDirection = Camera.main.transform.TransformDirection(movement);
+        moveDirection.y = 0f;
+
+        characterController.Move(moveDirection * speed * Time.deltaTime);
+    }
+
 }
