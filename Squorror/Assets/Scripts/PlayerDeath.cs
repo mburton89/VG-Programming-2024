6c496bd9a389ac70fd15fef4bf8c609fd384130d
SnackAttack24-7 <128613056+SnackAttack24-7@users.noreply.github.com>
2024-09-24 20:57:01 -0500

    Added playerdeath, and retry.
    Added playerdeath, and retry scripts.  Working retry button in the ui. The ui enables and disables depending on if the player is alive or dead.

---
+++ Squorror/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDeath : PlayerMechanics
+{
+    bool isDead = false;
+
+    void Update()
+    {
+        HandleDeath();
+        HandleUI();
+    }
+
+    public void HandleDeath()
+    {
+        if (currentHealth  <= 0)
+        {
+            isDead = true;
+        }
+    }
+    public void HandleUI()
+    {
+        if (isDead == false)
+        {
+            GameObject.Find("UI").SetActive(false);
+        }
+
+        else
+        {
+            GameObject.Find("UI").SetActive(true);
+        }
+    }
+}
