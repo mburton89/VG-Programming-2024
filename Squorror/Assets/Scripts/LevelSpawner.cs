c7faaf27989918ef7b0b6e86fda99ed16e49520d
KeatinWerleman <101309131+KeatinWerleman@users.noreply.github.com>
2024-10-08 19:12:25 -0500

    Added Level Spawner and New Levels
    Added a Level Spawner Script. If you ever want to spawn a new level, use LevelSpawner.Instance.SpawnLevel()

---
+++ Squorror/Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour
+{
+    public static LevelSpawner Instance;
+    public List<GameObject> levelLayouts;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnLevel()
+    {
+        Debug.Log("Start Level Spawn");
+        int rand = Random.Range(0, levelLayouts.Count + 1);
+        GameObject spawnedLevel = levelLayouts[rand];
+        spawnedLevel.SetActive(true);
+        Debug.Log("Spawned Level " + rand);
+
+
+    }
+}
