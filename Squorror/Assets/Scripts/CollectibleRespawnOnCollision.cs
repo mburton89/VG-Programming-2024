9d2dc5d1e3167fcf455a1a3cbb1247209c9f2a22
KeatinWerleman <101309131+KeatinWerleman@users.noreply.github.com>
2024-10-08 20:29:52 -0500

    10/8 final v

--- Squorror/Assets/Scripts/CollectibleRespawnOnCollision.cs
+++ Squorror/Assets/Scripts/CollectibleRespawnOnCollision.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollectibleRespawnOnCollision : MonoBehaviour
 {
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Floor" && collision.gameObject.tag != "Agent")
